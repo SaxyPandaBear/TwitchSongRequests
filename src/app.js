999c07ace39518f7d120c6e52a356ce510783ca8
Andrew Huynh <andrew.huynh@bti360.com>
2020-08-31 21:28:43 -0400

    add missing files

---
+++ src/app.js
@@ -0,0 +1,8 @@
+/**
+ * Main server code for handling requests from the UI. This should serve some APIs, 
+ * the main one being an API that accepts data in order to initiate the workflow for 
+ * listening to a Twitch channel. The event handler for listening to the twitch events
+ * will handle processing the data.
+ */
+
+ console.log("Hello, world!"); // TODO: got a lot of work to do.
