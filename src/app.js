2170f86ae43cec63e8d2df57eab94662ad41cb30
Andrew Huynh <andrew.huynh@bti360.com>
2020-09-02 18:40:32 -0400

    started working on main server code

--- src/app.js
+++ src/app.js
@@ -5,4 +5,47 @@
  * will handle processing the data.
  */
 
- console.log("Hello, world!"); // TODO: got a lot of work to do.
+// TODO: figure out how to get dynamic properties set up so that it can run locally
+//       and deployed
+const properties = {
+    "env": "LOCAL"
+}
+
+const express = require("express");
+require("auth.js")();
+var app = express();
+
+/**
+ * Handles validating the contents of a request that contains authentication details.
+ * This is used for both Spotify and Twitch authorization handling.
+ * 
+ * We expect the body to contain an `authorization_code`
+ * 
+ * TODO: see if there's anything else needed
+ * @param {*} body 
+ * @returns true if authorization_code is in body, false otherwise
+ */
+function validateAuthRequestBody(body) {
+    return "authorization_code" in body;
+}
+
+/**
+ * Upserts the Twitch authentication details into the database,
+ * and then starts a WebSocket connection to Twitch. 
+ * 
+ * This is meant to be asynchronous. The client-side UI should poll
+ * the GET endpoint to check the status of the 
+ */
+app.post("/twitch", function (req, res) {
+    if (!validateAuthRequestBody(req.body)) {
+        res.status(400).send("Missing 'authorization_code' in request body");
+    }
+});
+
+app.post("/spotify", function (req, res) {
+    if (!validateAuthRequestBody(req.body)) {
+        res.status(400).send("Missing 'authorization_code' in request body");
+    }
+});
+
+app.listen(8080);
---
+++ src/auth.js
@@ -0,0 +1,13 @@
+/**
+ * Handles Twitch and Spotify Oauth token fetching
+ */
+
+module.exports = function() {
+    this.fetchTwitchToken = function(clientId, clientSecret, authorizationCode) {
+
+    }
+
+    this.fetchSpotifyToken = function(clientId, clientSecret, authorizationCode) {
+        
+    }
+}
