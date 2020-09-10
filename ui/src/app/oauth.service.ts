9a7305179a7c4343af1e3373c07bdbe54aa3f976
Eugene Vedensky <Eugene.Vedensky@gmail.com>
2020-09-10 19:04:13 -0400

    removing commented code

--- ui/src/app/oauth.service.ts
+++ ui/src/app/oauth.service.ts
@@ -1,42 +1,37 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: "root",
+    providedIn: 'root',
 })
 export class OauthService {
-  endpoint = "http://localhost:8080/api/session";
-  spotifyEndpoint = `${this.endpoint}/spotify`;
-  twitchcEndpoint = `${this.endpoint}/twitch`;
-  accessKeysEndpoint = `${this.endpoint}/access-keys`;
+    endpoint = 'http://localhost:8080/api/session';
+    spotifyEndpoint = `${this.endpoint}/spotify`;
+    twitchcEndpoint = `${this.endpoint}/twitch`;
+    accessKeysEndpoint = `${this.endpoint}/access-keys`;
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-  getOauthStatus() {
-    return this.http.get(this.accessKeysEndpoint, { withCredentials: true });
-  }
-  setSpotifyAcessKey(spotifyAccessKey: string) {
-    return this.http.post(
-      this.spotifyEndpoint,
-      {
-        accessKey: spotifyAccessKey,
-      },
-      { withCredentials: true }
-    );
-  }
-  // setTwitchAccessKey(twitchAccesKey: string, channelId) {
-  //   return this.http.post(
-  //     this.twitchcEndpoint,
-  //     { accessKeysAndChannelId: { accessKey: twitchAccesKey, channelId } },
-  //     { withCredentials: true }
-  //   );
-  // }
+    getOauthStatus() {
+        return this.http.get(this.accessKeysEndpoint, {
+            withCredentials: true,
+        });
+    }
+    setSpotifyAcessKey(spotifyAccessKey: string) {
+        return this.http.post(
+            this.spotifyEndpoint,
+            {
+                accessKey: spotifyAccessKey,
+            },
+            { withCredentials: true }
+        );
+    }
 
-  setTwitchAccessKey(twitchCode: string) {
-    return this.http.post(
-      this.twitchcEndpoint,
-      { accessKey: twitchCode },
-      { withCredentials: true }
-    );
-  }
+    setTwitchAccessKey(twitchCode: string) {
+        return this.http.post(
+            this.twitchcEndpoint,
+            { accessKey: twitchCode },
+            { withCredentials: true }
+        );
+    }
 }
