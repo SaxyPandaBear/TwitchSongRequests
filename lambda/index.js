dcd43f9dd7743de5778494a691a69b7acf0147f1
Andrew Huynh <andrew.huynh@bti360.com>
2020-09-16 22:52:06 -0400

    code cleanup

--- lambda/index.js
+++ lambda/index.js
@@ -3,7 +3,6 @@
  * a connected Spotify player.
  */
 const AWS = require('aws-sdk');
-AWS.config.update({ region: 'us-east-1' });
 const fetch = require('node-fetch');
 
 const TABLE_NAME = 'connections';
@@ -12,11 +11,11 @@ const TABLE_NAME = 'connections';
 // lambda container, we need the LOCALSTACK_HOSTNAME env variable
 let config = { apiVersion: '2012-08-10', region: 'us-east-1' }
 if ('LOCALSTACK_HOSTNAME' in process.env) {
-    console.log('======= RUNNING LAMBDA IN LOCALSTACK ======');
+    console.info('======= RUNNING LAMBDA IN LOCALSTACK ======');
+    AWS.config.update({ region: 'us-east-1' });
     let ep = new AWS.Endpoint(`http://${process.env['LOCALSTACK_HOSTNAME']}:4566`);
     config.endpoint = ep;
 }
-console.log(`AWS Configs: ${JSON.stringify(config)}`);
 var dynamo = new AWS.DynamoDB(config);
 
 /**
@@ -25,7 +24,6 @@ var dynamo = new AWS.DynamoDB(config);
  * @param {array} devices
  */
 function findFirstComputer(devices) {
-    console.log('findFirstComputer');
     let computers = devices.filter(
         (device) => device.type === 'Computer' && device.is_active
     );
@@ -38,7 +36,6 @@ function findFirstComputer(devices) {
 
 // Get all devices for a user
 async function getDevices(accessToken) {
-    console.log('getDevices');
     let response = await fetch('https://api.spotify.com/v1/me/player/devices', {
         method: 'GET',
         mode: 'cors',
@@ -52,7 +49,6 @@ async function getDevices(accessToken) {
 
 // queue a song URI for the given active player
 async function queueSong(oauth, device, uri) {
-    console.log('queueSong');
     let response = await fetch(
         `https://api.spotify.com/v1/me/player/queue?uri=${uri}&device_id=${device.id}`,
         {
@@ -82,16 +78,17 @@ function fetchConnectionDetails(channelId) {
         ConsistentRead: true,
         ProjectionExpression: 'sess, connectionStatus'
     };
-    console.log('Getting stuff from dynamo');
     return dynamo.getItem(params, function (err, data) {
         if (err) {
-            console.log(err);
+            console.error(err);
+            throw err;
         } else {
             return data.Item;
         }
     }).promise();
 }
 
+// TODO: add this to the implementation
 async function refreshSpotifyToken(clientId, clientSecret, refreshToken) {
     let request = {
         'grant_type': 'refresh_token',
@@ -137,41 +134,37 @@ exports.handler = async function (event, context, callback) {
     for (const record of event.Records) {
         // get the message attributes to figure out which channel this request
         // is for
-        console.log('Received a new record to process')
         let channelId = record.messageAttributes['channelId']['stringValue'];
         let spotifyUri = record.body;
 
-        console.log(`Channel ID is: ${channelId}`);
-        console.log(`Spotify URI is: ${spotifyUri}`);
 
-        const data = await fetchConnectionDetails(channelId);
-        console.log('Got our stuff from dynamo');
-        console.log(JSON.stringify(data));
+        const connectionDetails = await fetchConnectionDetails(channelId);
         // if the connection statis is not active, then we shouldn't try to queue
         // a song.
-        if (data.Item.connectionStatus.S !== 'active') {
-            console.log('User is not connected, dropping record');
+        if (connectionDetails.Item.connectionStatus.S !== 'active') {
+            console.info('User is not connected to the main service. Dropping record');
         } else {
             // need to parse the session object to get the spotify credentials
-            let sessionObj = JSON.parse(data.Item.sess.S);
+            let sessionObj = JSON.parse(connectionDetails.Item.sess.S);
             let accessToken = sessionObj.accessKeys.spotifyToken.access_token;
-            let refreshToken = sessionObj.accessKeys.spotifyToken.refresh_token;
+            let refreshToken = sessionObj.accessKeys.spotifyToken.refresh_token; // TODO: use refresh token
 
-            console.log(`Access token from Spotify: ${accessToken}`);
-            console.log(`Refresh token from Spotify: ${refreshToken}`);
-
-            const foundDevices = await getDevices(accessToken)
-            console.log(`GET devices responded with ${JSON.stringify(foundDevices)}`);
+            const foundDevices = await getDevices(accessToken);
             let devices = foundDevices.devices;
             let activeDevice = findFirstComputer(devices);
             if (activeDevice === null) {
-                console.log('No active device found. Write error to dynamo');
+                // TODO: after events table is implemented, write an error about this to the table
+                console.warn('There was no active player that Spotify could connect to');
             } else {
-                const queueResponse = await queueSong(accessToken, activeDevice, spotifyUri)
-                let message = `Successfully queued song. Spotify responded with ${JSON.stringify(queueResponse)}`
-                console.log(message);
+                const queueResponse = await queueSong(accessToken, activeDevice, spotifyUri);
+                if (error in queueResponse) {
+                    // TODO: after events table is implemented, write an error about this to the table
+                    console.warn(`Spotify responded with an error: ${queueResponse.error}`);
+                } else {
+                    console.info(`Successfully queued song. Spotify responded with ${JSON.stringify(queueResponse)}`);
+                }
             }
         }
     }
-    return 'Successfully queued songs!';
+    return 'Successfully processed all records';
 };
