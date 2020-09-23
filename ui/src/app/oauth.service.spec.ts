c4f7b6677c6303205b737e44d3fb3caebfb81ea2
Eugene Vedensky <eugene.vedensky@gmail.com>
2020-09-23 00:45:23 -0400

    fixing tests

--- ui/src/app/oauth.service.spec.ts
+++ ui/src/app/oauth.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OauthService } from './oauth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('OauthService', () => {
     let service: OauthService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
         service = TestBed.inject(OauthService);
     });
 
