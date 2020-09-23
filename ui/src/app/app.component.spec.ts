d1f21773c7c021a9d9c12fd58be0d4675dc7e862
Andrew Huynh <andrew.huynh@bti360.com>
2020-09-22 21:25:40 -0400

    remove bad test

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -16,18 +16,9 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'TwitchSongRequestsUi'`, () => {
+    it(`should have as title 'TwitchSongRequests'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('TwitchSongRequestsUi');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain(
-            'TwitchSongRequestsUi app is running!'
-        );
+        expect(app.title).toEqual('TwitchSongRequests');
     });
 });
