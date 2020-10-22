90e465528e7e7cb9d02db4584174cfd6d3e745a7
Andrew Huynh <andrew.huynh@bti360.com>
2020-10-22 00:06:37 -0400

    added metric collector class

---
+++ server/src/main/scala/com/github/saxypandabear/songrequests/metric/CloudWatchMetricCollector.scala
@@ -0,0 +1,41 @@
+package com.github.saxypandabear.songrequests.metric
+
+import java.util.Date
+
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch
+import com.amazonaws.services.cloudwatch.model.{Dimension, MetricDatum, PutMetricDataRequest, StandardUnit}
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.collection.JavaConverters._
+
+/**
+ * Simple implementation that takes data and publishes to CloudWatch
+ * @param client internal AWS SDK CloudWatch client
+ */
+class CloudWatchMetricCollector(client: AmazonCloudWatch) extends LazyLogging {
+    def emitCountMetric(name: String, value: Long, tags: Map[String, String] = Map.empty): Unit = {
+        val datum = new MetricDatum()
+            .withMetricName(name)
+            .withTimestamp(new Date())
+            .withUnit(StandardUnit.Count)
+            .withValue(value.toDouble)
+
+        if (tags.nonEmpty) {
+            datum.setDimensions(convertMapToDimensions(tags).asJava)
+        }
+
+        logger.info("Emitting new metric[{}={}]", name, value)
+        val request = new PutMetricDataRequest()
+            .withNamespace("TwitchSongRequests")
+            .withMetricData(datum)
+
+        val response = client.putMetricData(request)
+        logger.info("Response from emitting metric[{}={}]: {}", name, value, response)
+    }
+
+    private def convertMapToDimensions(tags: Map[String, String]): Seq[Dimension] = {
+        tags.map {
+            case (k, v) => new Dimension().withName(k).withValue(v)
+        }.toSeq
+    }
+}
