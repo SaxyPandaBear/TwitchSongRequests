615c88f9f417deb935173fec6b7ed1e5f06ea42e
Andrew Huynh <andrew.huynh@bti360.com>
2020-10-25 16:14:53 -0400

    defined metric names

--- server/src/main/scala/com/github/saxypandabear/songrequests/metric/CloudWatchMetricCollector.scala
+++ server/src/main/scala/com/github/saxypandabear/songrequests/metric/CloudWatchMetricCollector.scala
@@ -1,6 +1,7 @@
 package com.github.saxypandabear.songrequests.metric
 
 import java.util.Date
+import java.util.concurrent.ExecutorService
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch
 import com.amazonaws.services.cloudwatch.model.{Dimension, MetricDatum, PutMetricDataRequest, StandardUnit}
@@ -9,10 +10,14 @@ import com.typesafe.scalalogging.LazyLogging
 import scala.collection.JavaConverters._
 
 /**
- * Simple implementation that takes data and publishes to CloudWatch
+ * Simple implementation that takes data and publishes to CloudWatch.
+ * This should perform the operations asynchronously, because we expect
+ * to share this object throughout the app, and can't let this metric
+ * emitter be the bottleneck for processing data.
  * @param client internal AWS SDK CloudWatch client
  */
-class CloudWatchMetricCollector(client: AmazonCloudWatch) extends LazyLogging {
+class CloudWatchMetricCollector(client: AmazonCloudWatch, executorService: ExecutorService) extends LazyLogging {
+
     def emitCountMetric(name: String, value: Double, tags: Map[String, String] = Map.empty): Unit = {
         val datum = new MetricDatum()
             .withMetricName(name)
@@ -33,9 +38,20 @@ class CloudWatchMetricCollector(client: AmazonCloudWatch) extends LazyLogging {
         logger.info("Response from emitting metric[{}={}]: {}", name, value, response)
     }
 
+    def emitCountMetric(name: String, value: Long, tags: Map[String, String] = Map.empty): Unit = {
+        emitCountMetric(name, value.toLong, tags)
+    }
+
     private def convertMapToDimensions(tags: Map[String, String]): Seq[Dimension] = {
         tags.map {
             case (k, v) => new Dimension().withName(k).withValue(v)
         }.toSeq
     }
+}
+
+object MetricsConstants {
+    val SONG_REQUEST_RECEIVED = "received-song-request-count"
+    val SENT_TO_SQS_TOTAL = "total-send-sqs-count"
+    val SENT_TO_SQS_FAILED = "failed-send-sqs-count"
+    val OAUTH_TOKEN_REFRESHED = "token-refresh-count"
 }
