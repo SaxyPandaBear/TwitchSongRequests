3f87d6118ff336b2e8352cfbb8cf77ed33001c9c
Andrew Huynh <andrew.huynh@bti360.com>
2020-11-01 10:40:34 -0500

    format metric file

--- server/src/main/scala/com/github/saxypandabear/songrequests/metric/CloudWatchMetricCollector.scala
+++ server/src/main/scala/com/github/saxypandabear/songrequests/metric/CloudWatchMetricCollector.scala
@@ -4,7 +4,12 @@ import java.util.Date
 import java.util.concurrent.ExecutorService
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch
-import com.amazonaws.services.cloudwatch.model.{Dimension, MetricDatum, PutMetricDataRequest, StandardUnit}
+import com.amazonaws.services.cloudwatch.model.{
+  Dimension,
+  MetricDatum,
+  PutMetricDataRequest,
+  StandardUnit
+}
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.collection.JavaConverters._
@@ -17,44 +22,62 @@ import scala.collection.JavaConverters._
  * @param client          internal AWS SDK CloudWatch client
  * @param executorService thread pool executor to submit EmitMetricTasks to
  */
-class CloudWatchMetricCollector(client: AmazonCloudWatch, executorService: ExecutorService) {
+class CloudWatchMetricCollector(
+    client: AmazonCloudWatch,
+    executorService: ExecutorService
+) {
 
-    def emitCountMetric(name: String, value: Double, tags: Map[String, String] = Map.empty): Unit = {
-        executorService.submit(new EmitMetricTask(client, name, value, tags))
-    }
+  def emitCountMetric(
+      name: String,
+      value: Double,
+      tags: Map[String, String] = Map.empty
+  ): Unit =
+    executorService.submit(new EmitMetricTask(client, name, value, tags))
 }
 
-class EmitMetricTask(client: AmazonCloudWatch, name: String, value: Double, tags: Map[String, String]) extends Runnable with LazyLogging {
-    override def run(): Unit = {
-        val datum = new MetricDatum()
-            .withMetricName(name)
-            .withTimestamp(new Date())
-            .withUnit(StandardUnit.Count)
-            .withValue(value)
+class EmitMetricTask(
+    client: AmazonCloudWatch,
+    name: String,
+    value: Double,
+    tags: Map[String, String]
+) extends Runnable
+    with LazyLogging {
+  override def run(): Unit = {
+    val datum = new MetricDatum()
+      .withMetricName(name)
+      .withTimestamp(new Date())
+      .withUnit(StandardUnit.Count)
+      .withValue(value)
 
-        if (tags.nonEmpty) {
-            datum.setDimensions(convertMapToDimensions(tags).asJava)
-        }
-
-        logger.info("Emitting new metric[{}={}]", name, value)
-        val request = new PutMetricDataRequest()
-            .withNamespace("TwitchSongRequests")
-            .withMetricData(datum)
-
-        val response = client.putMetricData(request)
-        logger.info("Response from emitting metric[{}={}]: {}", name, value, response)
+    if (tags.nonEmpty) {
+      datum.setDimensions(convertMapToDimensions(tags).asJava)
     }
 
-    private def convertMapToDimensions(tags: Map[String, String]): Seq[Dimension] = {
-        tags.map {
-            case (k, v) => new Dimension().withName(k).withValue(v)
-        }.toSeq
-    }
+    logger.info("Emitting new metric[{}={}]", name, value)
+    val request = new PutMetricDataRequest()
+      .withNamespace("TwitchSongRequests")
+      .withMetricData(datum)
+
+    val response = client.putMetricData(request)
+    logger.info(
+        "Response from emitting metric[{}={}]: {}",
+        name,
+        value,
+        response
+    )
+  }
+
+  private def convertMapToDimensions(
+      tags: Map[String, String]
+  ): Seq[Dimension] =
+    tags.map { case (k, v) =>
+      new Dimension().withName(k).withValue(v)
+    }.toSeq
 }
 
 object MetricsConstants {
-    val SONG_REQUEST_RECEIVED = "received-song-request-count"
-    val SENT_TO_SQS_TOTAL = "total-send-sqs-count"
-    val SENT_TO_SQS_FAILED = "failed-send-sqs-count"
-    val OAUTH_TOKEN_REFRESHED = "token-refresh-count"
+  val SONG_REQUEST_RECEIVED = "received-song-request-count"
+  val SENT_TO_SQS_TOTAL     = "total-send-sqs-count"
+  val SENT_TO_SQS_FAILED    = "failed-send-sqs-count"
+  val OAUTH_TOKEN_REFRESHED = "token-refresh-count"
 }
