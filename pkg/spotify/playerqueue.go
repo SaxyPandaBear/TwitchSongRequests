1246d677f154bd63080c98d7aab6e40b47d174fc
Andrew Huynh <17056456+SaxyPandaBear@users.noreply.github.com>
2023-02-19 22:26:02 -0500

    queue song in player

--- pkg/spotify/playerqueue.go
+++ pkg/spotify/playerqueue.go
@@ -1,13 +1,18 @@
 package spotify
 
 import (
+	"context"
+	"errors"
 	"regexp"
 
 	"github.com/saxypandabear/twitchsongrequests/pkg/queue"
 	"github.com/zmb3/spotify/v2"
 )
 
-var openSpotifyURLPattern = regexp.MustCompile(`^https://open.spotify.com/track/([A-Za-z0-9]+)`)
+var (
+	openSpotifyURLPattern = regexp.MustCompile(`^https://open.spotify.com/track/([A-Za-z0-9]+)`)
+	ErrInvalidInput       = errors.New("invalid user input for Spotify URI")
+)
 
 // ensure struct implements queue.Publisher
 var _ queue.Publisher = (*SpotifyPlayerQueue)(nil)
@@ -18,7 +23,12 @@ type SpotifyPlayerQueue struct {
 // Publish will validate that the input matches a valid Spotify URL scheme,
 // and then attempt to queue it in the user's Spotify player.
 func (s *SpotifyPlayerQueue) Publish(client *spotify.Client, url string) error {
-	return nil
+	id := parseSpotifyTrackID(url)
+	if len(id) < 1 {
+		return ErrInvalidInput
+	}
+
+	return client.QueueSong(context.Background(), spotify.ID(id))
 }
 
 // parseSpotifyTrackID takes an input string and tries to match it to the URL that you
